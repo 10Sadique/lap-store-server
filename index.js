46c824814e0472df1bfe0c37bbd907eb9a9cc81c
10Sadique <jafarsjahan@gmail.com>
2022-11-26 01:32:11 +0600

    get product for payment

--- index.js
+++ index.js
@@ -66,6 +66,17 @@ async function run() {
             res.send(products);
         });
 
+        // Get product by id for payment
+        app.get('/payment/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id),
+            };
+            const product = await productCollection.findOne(query);
+
+            res.send(product);
+        });
+
         // Get products by seller email
         app.get('/products', async (req, res) => {
             const email = req.query.email;
@@ -137,7 +148,7 @@ async function run() {
         // Get seller by email
         app.get('/seller/:email', async (req, res) => {
             const email = req.params.email;
-            console.log(email);
+
             const query = {
                 email: email,
             };
