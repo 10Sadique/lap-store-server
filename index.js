bc40b961c847cf578e4f98e3efc412715104d500
10Sadique <jafarsjahan@gmail.com>
2022-11-27 14:39:48 +0600

    jwt in advertised products

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
         });
 
         // Get advertised products
-        app.get('/products/advertised', async (req, res) => {
+        app.get('/products/advertised', verifyJWT, async (req, res) => {
             const query = {
                 isAdvertised: true,
                 isSold: false,
