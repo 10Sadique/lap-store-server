a00e113aeb6deca90b2cca538d53b36b20c513dd
10Sadique <jafarsjahan@gmail.com>
2022-11-27 19:35:55 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ async function run() {
         });
 
         // Add user to usersCollection
-        app.post('/users', verifyJWT, async (req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
